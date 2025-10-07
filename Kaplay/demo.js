99886456c52232a77b2ee6d316a0e67b1dd7cf1a
climb2bit <rhorizonus@hotmail.com>
2025-10-07 21:21:59 +0800

    fixmissing css link

--- Kaplay/demo.js
+++ Kaplay/demo.js
@@ -1,6 +1,6 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 
-const gameContainer = document.getElementById("game-content");
+const gameContainer = document.getElementsByClassName("game-content")[0];
 
 // Initialize Kaplay, passing the div as the canvas parent
 const k = kaplay({
