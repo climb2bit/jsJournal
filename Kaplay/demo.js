c5515a44c6e75f8beb5b999861efa97cc23c946b
climb2bit <rhorizonus@hotmail.com>
2025-10-07 21:41:29 +0800

    fix size of window again

--- Kaplay/demo.js
+++ Kaplay/demo.js
@@ -1,13 +1,14 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 
-const gameContainer = document.getElementsByClassName("game-content")[0];
+const gameContainer = document.getElementById("canvas");
 
 // Initialize Kaplay, passing the div as the canvas parent
 kaplay({
     width: gameContainer.offsetWidth,  // Match div's width
     height: gameContainer.offsetHeight, // Match div's height
-    canvas: gameContainer,              // This is the key!
-    background: [0, 0, 0, 1],           // Black background
+    background: "#d46eb3",
+    scale: 2,
+    canvas: document.getElementById("canvas"),
 });
 
 const bag = add([
