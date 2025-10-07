ac6067c1a79c365c38abff82606c8be1749ea2e1
climb2bit <rhorizonus@hotmail.com>
2025-10-07 21:03:50 +0800

    try to fix js

--- Kaplay/demo.js
+++ Kaplay/demo.js
@@ -1,2 +1,23 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
-kaplay();
+
+const ui = document.querySelector(".game-nav");
+
+new ResizeObserver(() => {
+  document.documentElement.style.setProperty(
+    "--scale",
+    Math.min(
+      ui.parentElement.offsetWidth / ui.offsetWidth,
+      ui.parentElement.offsetHeight / ui.offsetHeight
+    )
+  );
+}).observe(ui.parentElement);
+
+const k = kaplay({
+  width: 640,
+  height: 480,
+  letterbox: true,
+});
+
+k.loadBean();
+
+k.add([k.sprite("bean"), k.anchor("center"), k.pos(k.center()), k.scale(2)]);
