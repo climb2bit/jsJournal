1e009c0474721f6ef10d4cdd4b176caab03c77e7
climb2bit <rhorizonus@hotmail.com>
2025-10-07 21:59:05 +0800

    finally fixed!

--- Kaplay/demo.js
+++ Kaplay/demo.js
@@ -1,16 +1,14 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 
-const gameContainer = document.getElementById("canvas");
+const gameContainer = document.getElementsByClassName("game-content")[0];
 
 // Initialize Kaplay, passing the div as the canvas parent
-kaplay({
-    width: gameContainer.offsetWidth,  // Match div's width
-    height: gameContainer.offsetHeight, // Match div's height
-    background: "#d46eb3",
-    scale: 2,
-    canvas: document.getElementById("canvas"),
+const k = kaplay({
+    width: gameContainer.offsetWidth-5,  // Match div's width
+    height: gameContainer.offsetHeight-5, // Match div's height
+    root: gameContainer,
 });
 
-const bag = add([
-    sprite("bag"),
-]);
+k.loadBean();
+
+k.add([k.sprite("bean"), k.anchor("center"), k.pos(k.center()), k.scale(2)]);
